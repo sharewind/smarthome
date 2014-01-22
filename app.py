db0645db6d1f83ae404e2d72e32cc011588c2114
顾振(技术中心) <zhengu@sohu-inc.com>
2014-01-22 10:06:59 +0800

    add main hander

--- app.py
+++ app.py
@@ -34,9 +34,96 @@ class Application(tornado.web.Application):
 
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("welcome to smart home!")
-        self.write(str(PiSocketHandlerHandler.pi_clients))
+	def get(self):
+		token = 'guzhen'
+		signature = self.get_argument('signature', None)
+		timestamp = self.get_argument('timestamp', None)
+		nonce = self.get_argument('nonce', None)
+		echostr = self.get_argument('echostr', None)
+		logging.info(token) 
+		logging.info( signature )
+		logging.info( timestamp )
+		logging.info( nonce )
+		logging.info( echostr)
+		tmpList = [token, timestamp, nonce]
+		tmpList.sort()
+		tmpstr = "%s%s%s" % tuple(tmpList)
+		hashstr = hashlib.sha1(tmpstr).hexdigest()
+		logging.info( hashstr)
+		if hashstr == signature:
+			self.finish(echostr) 
+		else:
+			self.finish('None')
+
+	def post(self):
+		msg = self.parse_msg()
+		logging.info( msg)
+		#设置返回数据模板	
+		#纯文本格式
+		textTpl = """<xml>
+		<ToUserName><![CDATA[%s]]></ToUserName>
+		<FromUserName><![CDATA[%s]]></FromUserName>
+		<CreateTime>%s</CreateTime>
+		<MsgType><![CDATA[%s]]></MsgType>
+		<Content><![CDATA[%s]]></Content>
+		<FuncFlag>0</FuncFlag>
+		</xml>"""
+		#图文格式
+		pictextTpl = """<xml>
+		<ToUserName><![CDATA[%s]]></ToUserName>
+		<FromUserName><![CDATA[%s]]></FromUserName>
+		<CreateTime>%s</CreateTime>
+		<MsgType><![CDATA[news]]></MsgType>
+		<ArticleCount>1</ArticleCount>
+		<Articles>
+		<item>
+		<Title><![CDATA[%s]]></Title>
+		<Description><![CDATA[%s]]></Description>
+		<PicUrl><![CDATA[%s]]></PicUrl>
+		<Url><![CDATA[%s]]></Url>
+		</item>
+		</Articles>
+		<FuncFlag>1</FuncFlag>
+		</xml> """
+		#判断Message类型，如果等于"Event"，表明是一个新关注用户
+		if msg["MsgType"] == "event":
+			if msg["Event"] == "subscribe":
+				# self.bind()
+				logging.info('text')
+				echostr = textTpl % (
+					msg['FromUserName'], msg['ToUserName'], str(int(time.time())), msg['MsgType'],
+					u"欢迎关注！")
+			else:
+				# self.unbind()
+				echostr = None
+		else:
+			logging.info('pictext')
+			echostr = pictextTpl % (msg['FromUserName'], msg['ToUserName'], str(int(time.time())), ' ', ' ', ' ', ' ')
+
+		logging.info(echestr)
+		self.finish(echostr) 
+
+	def parse_msg(self):
+		"""
+		这里是用来解析微信Server Post过来的XML数据的，取出各字段对应的值，以备后面的代码调用，也可用lxml等模块。
+		"""
+		recvmsg = self.request.body
+		logging.info( recvmsg)
+		root = ET.fromstring(recvmsg)
+		logging.info( root)
+		msg = {}
+		for child in root:
+			logging.info( child)
+			msg[child.tag] = child.text
+		return msg
+
+	def bind(self, pid, sn):
+
+		return
+
+	def unbind(self, pid):
+
+		return
 
 
 class PiSocketHandlerHandler(tornado.websocket.WebSocketHandler):
@@ -157,6 +244,7 @@ def main():
     tornado.options.parse_command_line()
     app = Application()
     app.listen(options.port)
+    tornado.autoreload.start(io_loop=None, check_time=500)
     tornado.ioloop.IOLoop.instance().start()
 
 
