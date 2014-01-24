79a6ceae05044741eb85845c53e4d9456464108b
root <root@raspberrypi.(none)>
2014-01-24 10:24:57 +0000

    1.Add Open Close Action.

--- client.py
+++ client.py
@@ -13,9 +13,27 @@ import datetime
 import time
 import redis
 import sys
+try:
+  import RPi.GPIO as GPIO
+except RuntimeError:
+  print("Error importing RPi.GPIO!\n")
+  print("This is probably because you need superuser privileges.\n")
+  print("You can achieve this by using 'sudo' to run your script.\n")
 from mdns_util import MDNS
 
 from tornado.options import define, options
+
+PIN_1 = 8
+PIN_2 = 10
+PIN_3 = 12
+PIN_4 = 16
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(PIN_1, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(PIN_2, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(PIN_3, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(PIN_4, GPIO.OUT, initial = GPIO.LOW)
 
 define("port", default=4444, help="run on the given port", type=int)
 
@@ -141,10 +159,16 @@ def my_on_message(message):
 		return
 
 	elif "open" == message:
-		return
+		GPIO.output(PIN_1, GPIO.HIGH)
+		GPIO.output(PIN_2, GPIO.HIGH)
+		GPIO.output(PIN_3, GPIO.HIGH)
+		GPIO.output(PIN_4, GPIO.HIGH)
 
 	elif "close" == message:
-		return
+		GPIO.output(PIN_1, GPIO.LOW)
+		GPIO.output(PIN_2, GPIO.LOW)
+		GPIO.output(PIN_3, GPIO.LOW)
+		GPIO.output(PIN_4, GPIO.LOW)
 
 	elif message.startswith('bindair'):
 		id = message[7:]
