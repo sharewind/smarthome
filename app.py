bdf273eaf94ee90ac5e98419142a9a5017360781
顾振(技术中心) <zhengu@sohu-inc.com>
2014-01-24 21:34:59 +0800

    fix

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ class MainHandler(tornado.web.RequestHandler):
 		if pi_id:
 			msgid = cache.get('MsgId:' + msg['MsgId'])
 			if msgid:
-				logging.info('2 MsgId:' + str(msg['MsgId'])
+				logging.info('2 MsgId:' + str(msg['MsgId']))
 				result = cache.get("pi_msg:" + pi_id + ':' + action)
 				if result:
 					return result
@@ -135,7 +135,7 @@ class MainHandler(tornado.web.RequestHandler):
 					time.sleep(5)
 					return 'time out'
 			else:
-				logging.info('1 MsgId:' + str(msg['MsgId'])
+				logging.info('1 MsgId:' + str(msg['MsgId']))
 				cache.setex('MsgId:' + msg['MsgId'], msg['MsgId'], 10)
 				PiSocketHandler.send_message(wx_id, msg)
 				time.sleep(5)
