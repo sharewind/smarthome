3105063ec0eeb376d9077a7bc3aace1a2b95cb34
顾振(技术中心) <zhengu@sohu-inc.com>
2014-01-24 23:03:01 +0800

    remove log

--- app.py
+++ app.py
@@ -258,7 +258,7 @@ class MainHandler(tornado.web.RequestHandler):
 		except:
 			logging.error('index is not int', exc_info=True)
 			result = "please input int"
-		logging.info('airbind:result:' + result)
+		# logging.info('airbind:result:' + result)
 		return result
 		
 
